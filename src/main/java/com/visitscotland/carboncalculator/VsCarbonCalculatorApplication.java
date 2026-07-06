151d8f37dabd15a0aadb92f10fe176ca9f4bd093
jose.calcines <jose.calcines@visitscotland.com>
2026-07-06 16:15:26 +0100

    BDATA-38 - Setting up the project

---
+++ src/main/java/com/visitscotland/carboncalculator/VsCarbonCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.visitscotland.carboncalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsCarbonCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VsCarbonCalculatorApplication.class, args);
+    }
+
+}
