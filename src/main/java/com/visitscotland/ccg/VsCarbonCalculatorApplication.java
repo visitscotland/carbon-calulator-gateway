dc3bde476b1235c0ff9f19774c3d31e5d4e93b8f
jose.calcines <jose.calcines@visitscotland.com>
2026-07-21 17:53:42 +0100

    BDATA-91 - Add The ConfigurationScan mechanism

--- src/main/java/com/visitscotland/ccg/VsCarbonCalculatorApplication.java
+++ src/main/java/com/visitscotland/ccg/VsCarbonCalculatorApplication.java
@@ -2,8 +2,10 @@ package com.visitscotland.ccg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class VsCarbonCalculatorApplication {
 
     public static void main(String[] args) {
