4a01fa3ca49e181ea65c576e6483d843307130c6
jose.calcines <jose.calcines@visitscotland.com>
2026-07-23 16:12:31 +0100

    BDATA-98 - Refactor the payload transformation in an external class

---
+++ src/main/java/com/visitscotland/ccg/payload/SubmissionPayloadTransformer.java
@@ -0,0 +1,49 @@
+package com.visitscotland.ccg.payload;
+
+import org.springframework.stereotype.Service;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.node.ObjectNode;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SubmissionPayloadTransformer {
+
+    public ObjectNode transform(JsonNode payload, String submissionId, String[] removeFields) {
+        ObjectNode modifiedPayload = payload.deepCopy().asObject();
+
+        for (String property : removeFields) {
+            modifiedPayload.remove(property);
+        }
+
+        modifiedPayload.put("vsUID", submissionId);
+
+        normalizeVariantFields(modifiedPayload);
+
+        return modifiedPayload;
+    }
+
+    private static final String DELIMITER = "---";
+
+    /**
+     * Normalises variant fields. Fields following the convention {@code <field>-<variant>} are converted to {@code <field>}
+     * If both the base field and one or more variants exist, the base field takes precedence and the variants are discarded.
+     */
+    public void normalizeVariantFields(ObjectNode modifiedPayload) {
+
+        List<String> variants = modifiedPayload.propertyNames().stream()
+                .filter(propertyName -> propertyName.contains(DELIMITER))
+                .collect(Collectors.toList());
+
+        for (String variant: variants) {
+            JsonNode value = modifiedPayload.get(variant);
+            if (!value.isNull() && !value.isEmpty()) {
+                modifiedPayload.putIfAbsent(variant.substring(0, variant.lastIndexOf(DELIMITER)), value);
+            }
+            modifiedPayload.remove(variant);
+        }
+    }
+
+
+}
