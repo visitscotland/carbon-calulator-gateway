2a1fbc60da183ae7aacbbb58924ecdf333c19297
jose.calcines <jose.calcines@visitscotland.com>
2026-07-23 16:16:31 +0100

    BDATA-67 - Enhance notification email output

---
+++ src/main/java/com/visitscotland/ccg/config/EmailProperties.java
@@ -0,0 +1,35 @@
+package com.visitscotland.ccg.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "notification.email")
+public class EmailProperties {
+
+    private String[] recipients;
+    private String subject;
+    private boolean enabled;
+
+    public String[] getRecipients() {
+        return recipients;
+    }
+
+    public void setRecipients(String[] recipients) {
+        this.recipients = recipients;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
--- src/main/java/com/visitscotland/ccg/notification/EmailNotificationService.java
+++ src/main/java/com/visitscotland/ccg/notification/EmailNotificationService.java
@@ -1,5 +1,8 @@
 package com.visitscotland.ccg.notification;
 
+import com.visitscotland.ccg.config.EmailProperties;
+import com.visitscotland.ccg.exception.TraceApiException;
+import com.visitscotland.ccg.exception.VsException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.MailException;
@@ -12,19 +15,65 @@ public class EmailNotificationService {
 
     private static final Logger logger = LoggerFactory.getLogger(EmailNotificationService.class);
 
-
+    private final EmailProperties properties;
     private final JavaMailSender mailSender;
 
-    public EmailNotificationService(JavaMailSender mailSender) {
+    public EmailNotificationService(EmailProperties properties, JavaMailSender mailSender) {
+        this.properties = properties;
         this.mailSender = mailSender;
     }
 
-    public void notify(String messageText, String subject, String[] formEmailRecipient) {
+    public void notify(TraceApiException traceApiException, VsException vsException, String submissionId) {
+        if (properties.isEnabled()) {
+            String message = compose(traceApiException, vsException, submissionId);
+            send(message);
+        }
+    }
+
+    private String compose(TraceApiException traceApiException, VsException vsException, String submissionId) {
+        StringBuilder message = new StringBuilder();
+        message.append("<p>There has been an error while processing the Registration for the Carbon Calculator</p>");
+        message.append("<ol>");
+
+        if (traceApiException != null) {
+            message.append("<li>")
+                    .append("Trace API Responded with status code ")
+                    .append(traceApiException.getStatusCode())
+                    .append(" and message: ")
+                    .append(traceApiException.getApiMessage())
+                    .append("</li>");
+        }
+
+        if (vsException != null) {
+            message.append("<li>")
+                    .append("BREG responded with the error message: ")
+                    .append(vsException.getMessage())
+                    .append("</li>");
+        }
+        message.append("</ol>");
+
+        message.append("<p>");
+        if (traceApiException == null || vsException == null) {
+            message.append("Note: <i>The submission is recoverable buy it needs to be manually processed.</i>");
+        } else {
+            message.append("Note: <strong> The submission is not recoverable as none of the service were able to process the data</strong>");
+        }
+
+        message.append("<p>The submission ID is ");
+        message.append(submissionId);
+        message.append("</p>");
+
+        logger.info(message.toString());
+
+        return message.toString();
+    }
+
+    public void send(String messageText) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
 
-            message.setTo(formEmailRecipient);
-            message.setSubject(subject);
+            message.setTo(properties.getRecipients());
+            message.setSubject(properties.getSubject());
             message.setText(messageText);
 
             mailSender.send(message);
