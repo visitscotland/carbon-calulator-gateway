5204e5b497e87fb0b15af4b3653c5a1eb4046185
jose.calcines <jose.calcines@visitscotland.com>
2026-07-27 12:18:16 +0100

    BDATA-38 - Add CORS configuration

---
+++ src/main/java/com/visitscotland/ccg/config/cors/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package com.visitscotland.ccg.config.cors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Value("${allowed_origin}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(allowedOrigins)
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
